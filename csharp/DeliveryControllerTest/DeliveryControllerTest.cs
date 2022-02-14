537c5686937f201da4486edec0a67659b3a8e37a
Thibaut Cantet <thibaut.cantet@soat.fr>
2022-02-14 13:46:47 +0100

    Should send an email when current delivery is not scheduled

--- csharp/DeliveryControllerTest/DeliveryControllerTest.cs
+++ csharp/DeliveryControllerTest/DeliveryControllerTest.cs
@@ -10,12 +10,28 @@ namespace DeliveryControllerTest
     {
         private readonly EmailGateway _emailGateway = new FakeEmailGateway();
         [Fact]
-        public void Dont_send_an_email_when_no_delivery_schedule()
+        public void Should_not_send_an_email_when_no_delivery_schedule()
         {
             List<Delivery> deliverySchedule = new List<Delivery>();
             var deliveryController = new DeliveryController.DeliveryController(deliverySchedule, _emailGateway);
 
             deliveryController.UpdateDelivery(new DeliveryEvent("id", DateTime.Now, new Location(0, 0)));
+            
+            Assert.False(((FakeEmailGateway) _emailGateway).HasBeenSent);
+        }
+        
+        [Fact]
+        public void Should_send_an_email_when_current_delivery_is_not_scheduled()
+        {
+            List<Delivery> deliverySchedule = new List<Delivery>() { new Delivery("id", 
+                "contact@email.com",
+                new Location(0f, 0f),
+                DateTime.Now,
+                false,
+                false) };
+            var deliveryController = new DeliveryController.DeliveryController(deliverySchedule, _emailGateway);
+
+            deliveryController.UpdateDelivery(new DeliveryEvent("anotherId", DateTime.Now, new Location(0, 0)));
             
             Assert.False(((FakeEmailGateway) _emailGateway).HasBeenSent);
         }
